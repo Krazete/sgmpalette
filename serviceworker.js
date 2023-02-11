6b6ccad2421486adb400743f446f7e2244e2dc20
Ted Michael Celis <krazete@gmail.com>
2023-02-11 11:30:27 -0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -2,47 +2,16 @@ const VERSION = "5.2.0"; /* update whenever anything changes */
 
 self.addEventListener("install", event => {
     // console.log("PWA Install: " + VERSION);
+    self.skipWaiting();
     event.waitUntil((async () => {
-        const assets = [
-            "./",
-            "./icon/ArrowGold.png",
-            "./icon/character_symbol_annie01.png",
-            "./icon/character_symbol_beowulf01.png",
-            "./icon/character_symbol_bigband01.png",
-            "./icon/character_symbol_blackdahlia01.png",
-            "./icon/character_symbol_cerebella01.png",
-            "./icon/character_symbol_custom01.png",
-            "./icon/character_symbol_double01.png",
-            "./icon/character_symbol_eliza01.png",
-            "./icon/character_symbol_filia01.png",
-            "./icon/character_symbol_fukua01.png",
-            "./icon/character_symbol_marie01.png",
-            "./icon/character_symbol_msfortune01.png",
-            "./icon/character_symbol_painwheel01.png",
-            "./icon/character_symbol_parasoul01.png",
-            "./icon/character_symbol_peacock01.png",
-            "./icon/character_symbol_robofortune01.png",
-            "./icon/character_symbol_scribble01.png",
-            "./icon/character_symbol_squigly01.png",
-            "./icon/character_symbol_umbrella01.png",
-            "./icon/character_symbol_valentine01.png",
-            "./icon/CheckmarkBad.png",
-            "./icon/CheckmarkGood.png",
-            "./icon/kofi.png",
-            "./icon/notblight.png",
-            "./texture/Space.png",
-            "./texture/fire.png",
-            "./texture/killarainbow.png",
-            "./texture/umbrella_veins.png",
-            "./texture/water.png"
-          ];
         const cache = await caches.open(VERSION);
-        cache.addAll(assets);
+        cache.addAll(["./"]);
     })());
 });
 
 self.addEventListener("activate", event => {
     // console.log("PWA Activate: " + VERSION);
+    self.clients.claim();
     event.waitUntil((async () => {
         const keys = await caches.keys();
         keys.forEach(async (key) => {
