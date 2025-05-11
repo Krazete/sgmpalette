8ee8120cb054788b71c6b1329efb4ef4155b8e70
Ted Michael Celis <krazete@gmail.com>
2025-05-11 12:43:53 -0700

    gushen

--- fix_custom_filenames.sh
+++ fix_custom_filenames.sh
@@ -3,18 +3,23 @@ IFS=$'\n'; set -f
 for i in $(find custom -name *.png); do
     while [[ $i =~ (.*)а(.*) ]]; do
         j=${BASH_REMATCH[1]}a${BASH_REMATCH[2]}
-        mv "$i" "$j.png"
+        mv "$i" "$j"
         i=$j
         echo "Cyrillic а replaced in $i"
+        sleep .1
     done
     while [[ $i =~ (.*)е(.*) ]]; do
         j=${BASH_REMATCH[1]}e${BASH_REMATCH[2]}
-        mv "$i" "$j.png"
+        mv "$i" "$j"
         i=$j
         echo "Cyrillic е replaced in $i"
+        sleep .1
     done
     if [[ $i =~ (.+_(raw|line|base|area|highlight|shadow)).+\.png ]]; then
         j=${BASH_REMATCH[1]}
+        while [[ -f "$j.png" ]]; do
+            j=$j+
+        done
         mv "$i" "$j.png"
         echo "$i -> $j.png"
     fi
