031a54216536566e861bc24589cd969c3dee59b8
Ted Michael Celis <krazete@gmail.com>
2025-05-31 13:13:36 -0700

    put dupe sprites back in; expanded blacklist instead
    and rewrote update_directory
    now i can have multiple subdirectories if i want to, not just custom

--- update_directory.py
+++ update_directory.py
@@ -1,32 +1,38 @@
 import os
 import json
+import re
 
 blacklist = [
-    'demiiial/marie', # color map too messy; index overlap with face and hair
-    'krazete/n_ko_avatar_filia' # cropped; replaced with full version by n_ko
+    'cat', # easter egg; randomly added by index.js instead
+    'Beowulf_AA2_GiganticArm', # duplicate of Beowulf_BB1_T1_GiganticArm
+    'Eliza_Debonaire', # duplicate of Eliza_NobleMogul with swapped line & detail channels
+    'Marie_BB1_GiganBeam', # duplicate of BlackDahlia_BB1_DoorMat
+    'Marie_BB3_FistFromAbove', # duplicate of BlackDahlia_BB3_Empower
+    'custom/demiiial/marie', # color map too messy; index overlap with face and hair
+    'custom/krazete/n_ko_avatar_filia', # cropped; replaced with full version by n_ko
 ]
 
 def update_directory():
     '''Updates directory.js with contents of sprite folder.'''
     directory = {}
 
-    for path in os.listdir('sprite'):
-        if os.path.isdir(path) or path == 'cat.png':
-            continue
-        filename = path.split('.')[0]
-        character = filename.split('_')[0].lower()
-        if character == 'heavymetal':
-            character = 'bigband'
-        directory.setdefault(character, [])
-        directory[character].append(filename)
-
-    directory.setdefault('custom', [])
-    for path in os.listdir('sprite/custom'):
-        for subpath in os.listdir('sprite/custom/{}'.format(path)):
-            name = '{}/{}'.format(path, '.'.join(subpath.split('.')[0:-1]))
-            if name in blacklist:
+    for root, dirs, files in os.walk('sprite'):
+        root = re.sub(r'\\', '/', root)
+        for file in files:
+            filepath = '/'.join(filter(None, [
+                re.sub(r'^sprite/?', '', root),
+                '.'.join(file.split('.')[0:-1])
+            ]))
+            if filepath in blacklist:
                 continue
-            directory['custom'].append('custom/{}'.format(name))
+            if root == 'sprite':
+                character = filepath.split('_')[0].lower()
+                if character == 'heavymetal':
+                    character = 'bigband'
+            elif '/' in root:
+                character = root.split('/')[1]
+            directory.setdefault(character, [])
+            directory[character].append(filepath)
 
     with open('directory.js', 'w') as fp:
         fp.write('var ids = ')
